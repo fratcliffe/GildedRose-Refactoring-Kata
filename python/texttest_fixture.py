9ab3c72126c8bc79e1e83c9ba51f39c6813e0c1a
Felicity Ratcliffe <felicity.ratcliffe@anaplan.com>
2022-03-12 15:14:49 +0000

    Test more cases for longer in TextTest tests

--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
              Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=12, quality=20),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=7, quality=40),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=3, quality=45),
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 
-    days = 2
+    days = 5
     import sys
     if len(sys.argv) > 1:
         days = int(sys.argv[1]) + 1
