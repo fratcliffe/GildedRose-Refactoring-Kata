2b79454ec15232da23b7df48946b445529b1260e
Felicity Ratcliffe <felicity.ratcliffe@anaplan.com>
2022-03-12 14:51:17 +0000

    Add tests for all item attributes

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -13,6 +13,18 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
-        
+    def test_bar(self):
+        items = [Item("bar", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].sell_in)
+
+    def test_baz(self):
+        items = [Item("baz", 0, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
